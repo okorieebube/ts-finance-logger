cf19f202789d3fe7fa91e8e699eeda3106735a60
Ebube <okorieebube1@gmail.com>
2022-04-06 09:25:21 +0100

    Classes implementing interfaces

---
+++ public/app.js
@@ -0,0 +1,19 @@
+import { Invoice } from "./classes/Invoice.js";
+import { Payment } from "./classes/Payment.js";
+const form = document.querySelector(".new-item-form");
+// input
+const type = document.querySelector("#type");
+const toFrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let doc;
+    if (type.value === "invoice") {
+        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
+});
